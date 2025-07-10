1ad6615a8ebd3635b7426d38fe0989810f88cd51
PENDOL10 <fendyrarestu2004@gmail.com>
2025-07-10 18:16:00 +0700

    Fix error vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/PORTOFOLIO-WEB/",
+  // Hapus baris base: "/PORTOFOLIO-WEB/",
+  // atau ubah menjadi:
+  base: "/", // Ini menunjukkan aplikasi di-deploy di root
   build: {
-    outDir: 'build', 
+    outDir: "build",
   },
   plugins: [react()],
-})
+});
